f531b4c1d961069961f8efacbb4214e85e2dad66
simonebuscaino <aulapcm-busca@users.noreply.github.com>
2019-11-08 15:03:07 +0100

    servizio finito

--- src/app/idea/services/pouchdb.service.ts
+++ src/app/idea/services/pouchdb.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { PouchDB } from 'pouchdb';
+import { Idea } from '../models/idea';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PouchdbService {
+  localDB: PouchDB;
 
-  constructor() { }
+  constructor() {
+    this.localDB = new PouchDB('idea');
+    const remoteDB = new PouchDB('http://localhost:5984/idea');
+  }
+
+  getIdea(id: string): Promise<Idea> {
+    return this.localDB.get(id);
+  }
+
+  fetchIdea(): Promise<Idea[]> {
+    return this.localDB
+      .allDocs({ include_docs: true })
+      .then(response => response.rows
+      .map(item => item.doc));
+  }
+
+  setIdea(idea: Idea): Promise<any> {
+    if (idea._id) {
+      return this.updateIdea(idea);
+    } else {
+      return this.createIdea(idea);
+    }
+  }
+
+  private createIdea(idea: Idea): Promise<any> {
+    return this.localDB.post(idea);
+  }
+
+  private updateIdea(idea: Idea): Promise<any> {
+    return this.localDB.put(idea);
+  }
+
+  deleteIdea(idea: Idea): Promise<any> {
+    return this.localDB.remove(idea);
+  }
 }
